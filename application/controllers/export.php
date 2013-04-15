21855344ec0455ea5365bbff1f33c89a2a129426
Taufiq Ridha <taufiq.ridha@gmail.com>
2013-04-15 14:38:16 +0700

    import all data 4

--- application/controllers/export.php
+++ application/controllers/export.php
@@ -639,6 +639,7 @@ class Export_Controller extends Base_Controller {
 		for($i=1;$i<=$freepasscount;$i++){
 			$inarray[0] = $i;
 			$inarray[1] = $data['freepassname'.$i.''];
+			$inarray[2] = $data['freepassname'.$i.'regnumber'];
 			$result[] = implode(',',$inarray);
 		}
 
@@ -650,6 +651,7 @@ class Export_Controller extends Base_Controller {
 		for($i=1;$i<=$boothassistantcount;$i++){
 			$inarray[0] = $i;
 			$inarray[1] = $data['boothassistant'.$i.''];
+			$inarray[2] = $data['boothassistant'.$i.'regnumber'];
 			$result[] = implode(',',$inarray);
 		}
 
