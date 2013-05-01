bc5e3354bb150b5de48f394b12e929ebc5532b2d
taufiqridha <taufiq.ridha@gmail.com>
2013-05-01 20:52:17 +0700

    skk update3

--- application/views/exhibitor/importbothassistant.blade.php
+++ application/views/exhibitor/importbothassistant.blade.php
@@ -42,7 +42,7 @@
   if(isset($boothassistantdata['exhibitorid']) && $boothassistantdata['exhibitorid']== "514c629e8dfa19db46000000"){
     $boothassistantcounter = 20;
   }elseif(isset($boothassistantdata['exhibitorid']) && $boothassistantdata['exhibitorid']== "516267808dfa19911e000000"){
-    $boothassistantcounter = 27;
+    $boothassistantcounter = 32;
   }else{
     $boothassistantcounter = 11;
   }
