3f4d3f8e430a0f1c3a6b4558a7e598942e2fdfd8
taufiqridha <taufiq.ridha@gmail.com>
2013-05-03 11:56:12 +0700

    closed

--- application/views/register/landing.blade.php
+++ application/views/register/landing.blade.php
@@ -3,6 +3,16 @@
 
 @section('content')
 <div class="tableHeader">
+<h3>Online Registration has been closed.</h3>
+<br/>
+<ul style="margin-left:25px;">
+<li> For those who wish registered after 30th April 2013, please contact conventionipa2013@dyandra.com or call 021 31997174</li>
+
+<li> Registration via phone will be open until 10th May 2013</li>
+
+<li> Onsite registration will be open on 13th - 17th May 2013</li>
+</ul>
+</p>
 <h3>{{$title}}</h3>
 </div>
 <!--{{ HTML::image('images/checked.png','checked',array('class'=>'check-icon','style'=>'float:left;')) }}-->
--- application/views/register/registrationclosed.blade.php
+++ application/views/register/registrationclosed.blade.php
@@ -8,7 +8,15 @@
 {{ HTML::image('images/locked.png','checked',array('class'=>'check-icon','style'=>'float:left;')) }}
 <h3>Sorry</h3>
 <p>Online Registration has been closed.
-<br/></p>
+<br/>
+<ul style="margin-left:25px;">
+<li> For those who wish registered after 30th April 2013, please contact conventionipa2013@dyandra.com or call 021 31997174</li>
+
+<li> Registration via phone will be open until 10th May 2013</li>
+
+<li> Onsite registration will be open on 13 - 17 May 2013</li>
+</ul>
+</p>
 
 
 @endsection
